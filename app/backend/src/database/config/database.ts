01ae850380e820cec7ef86e2a172f15c0aae4f2b
Lucas Toledo <lucasmoraisss@gmail.com>
2022-04-27 15:46:47 -0300

    feat: add volume

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -5,8 +5,8 @@ const config: Options = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '123456',
   database: process.env.DB_NAME || 'TRYBE_FUTEBOL_CLUBE',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  host: process.env.DB_HOST || 'db',
+  port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
